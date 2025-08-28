dd782452a8770da60b5110310afd0807f986115c
ksmith94 <102421938+ksmith94@users.noreply.github.com>
2025-08-28 11:39:35 -0400

    Add button variant

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -6,12 +6,13 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   text?: string;
   icon?: LucideIcon;
   iconPosition?: 'left' | 'right';
+  primary: boolean;
 }
 
 export function Button(props: ButtonProps): JSX.Element {
-  const { icon: Icon, iconPosition = 'left', text, children, ...rest } = props;
+  const { icon: Icon, iconPosition = 'left', text, children, primary, ...rest } = props;
   return (
-    <ButtonContainer {...rest}>
+    <ButtonContainer $primary={primary} {...rest}>
       {Icon && iconPosition === 'left' && <Icon />}
       {text}
       {children}
@@ -20,25 +21,34 @@ export function Button(props: ButtonProps): JSX.Element {
   );
 }
 
-const ButtonContainer = styled.button`
+const ButtonContainer = styled.button<{$primary?: boolean}>`
   width: fit-content;
-  /* padding: 0.1rem 0.5rem; */
   display: inline-flex;
   align-items: center;
   justify-content: center;
   height: 40px;
   gap: 0.5rem;
   padding: 0.6rem 1.2rem;
-  background-color: ${({ theme }) => theme.colors.primary.green500};
   font-size: ${({ theme }) => theme.fontSizes.base};
   font-family: ${({ theme }) => theme.fonts.main};
   color: white;
-  border: none;
   border-radius: 6px;
   cursor: pointer;
+  background-color: ${({ theme, $primary }) => $primary ?
+    theme.colors.primary.green500 :  
+    theme.colors.primary.green100
+  };
+  color: ${({ $primary, theme }) => $primary ? 
+    theme.colors.white :
+    theme.colors.primary.green900
+  };
+  border: ${({ $primary, theme }) => $primary ? 'none' : `2px solid ${theme.colors.primary.green900}`};
 
   &:hover {
-    background-color: ${({ theme }) => theme.colors.primary.green700};
+    background-color: ${({ theme, $primary }) => $primary ? 
+      theme.colors.primary.green700 :
+      theme.colors.primary.green200
+    };
   }
 
   svg {
